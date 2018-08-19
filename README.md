95fa7f0165d6aa590c5aeda27844714d64eef754
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-08-19 19:32:49 -0400

    Added omdb functionality

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Command line tool to practice fetching API data via node.
 ## How to Use
 In order to use this app, you must first supply API authentication in a .env file in the following format:
 
-```# Spotify API keys
+```
+# Spotify API keys
 
 SPOTIFY_ID=your-id
 SPOTIFY_SECRET=your-secret
@@ -19,9 +20,14 @@ REDDIT_PASSWORD=your-password
 
 # OMDB API keys
 
-OMDB_KEY=your-key```
+OMDB_KEY=your-key 
+```
 
 The app takes the third command line argument to determine its action. Any subsequent arguments may be action parameters.
 
 Type the follwing in your command line for a list of all available actions: 
 `node liri.js help`
+
+## Project History
+
+This project was a homework assignment for the Columbia Full Stack Web Development Coding Bootcamp, testing our preliminary ability to use nodejs. The project originally specified a gathering tweets instead of Reddit comments, but Twitter changed its API system two weeks before the project was due, so most students were unable to get an API key. 
--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
-var Spotify = require('node-spotify-api');
+const Spotify = require('node-spotify-api');
 const Reddit = require('snoowrap');
+const request = require('request');
 
 var keys = require('./keys.js');
 
@@ -10,14 +11,17 @@ var reddit = new Reddit(keys.reddit);
 if (process.argv[2] === "help") {
     console.log('Here are the currently available commands:')
     console.log();
-    console.log('node liri.js spotify-this-song "Song Name Here"');
+    console.log('node liri.js spotify-this-song "<song name here>"');
     console.log('--> Searches Spotify for a song with that title.');
     console.log();
     console.log('node liri.js my-comments #');
     console.log('--> Searches Reddit for user comments.');
     console.log('----> An integer argument is optional. If one is supplied, it sets the maximum number of results. It is 20 by default.')
     console.log();
-    
+    console.log('node liri.js movie-this "<movie name here>"');
+    console.log('--> Searches OMDB for a movie with that title.');
+    console.log();
+
     console.log('node liri.js help');
     console.log('--> Provides a list of available options. But you already knew that ;)');
 }
@@ -26,10 +30,12 @@ else if (process.argv[2] === "spotify-this-song") {
         .search({ type: 'track', query: process.argv[3] })
         .then(function (response) {
             let track = response.tracks.items[0];
+            console.log();
             console.log('Track: ' + track.name);
             console.log('Artist: ' + track.artists[0].name);
             console.log('Album: ' + track.album.name);
             console.log(track.href);
+            console.log();
         })
         .catch(function (err) {
             console.log(err);
@@ -41,8 +47,32 @@ else if (process.argv[2] === "my-comments") {
         var numberToDisplay = parseInt(process.argv[3]) || 20;
         if (numberToDisplay > res.length || numberToDisplay < 0 ) { numberToDisplay = res.length; }
         for(var i=0; i<numberToDisplay; i++){
+            console.log();
             console.log(res[i].body);
             console.log(new Date(new Date(parseInt(res[i].created_utc)*1000) + ' UTC'));
         }
     });
+}
+
+else if (process.argv[2] === "movie-this") {
+    request("https://www.omdbapi.com/?t=" + process.argv[3] + "&apikey=" + keys.omdb.key + "&r=json", function(err, res, body) {
+	if(!err && res.statusCode === 200) {
+        let movie = JSON.parse(body)
+        console.log();
+		console.log(movie.Title + ' (' + movie.Rated + ')');
+        console.log(movie.Year + ' - ' + movie.Country + ' (' + movie.Language + ')');
+        console.log();
+        console.log(movie.Plot);
+        console.log();
+        console.log("Starring...");
+        movie.Actors.split(', ').forEach(function(actor){
+            console.log('--> ' + actor);
+        });
+        console.log();
+		console.log("The critics say...");
+		movie.Ratings.forEach(function(rating){
+			console.log('--> ' + rating.Source + ": " + rating.Value)
+		})
+	}
+})
 }
