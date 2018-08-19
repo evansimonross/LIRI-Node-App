70c149f1b4191feda7c4dc644e0162cc0fdfa92a
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-08-19 19:16:54 -0400

    Added help function, some readme info, and an option limit to number of comments.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # LIRI-Node-App
 Command line tool to practice fetching API data via node.
+
+## How to Use
+In order to use this app, you must first supply API authentication in a .env file in the following format:
+
+```# Spotify API keys
+
+SPOTIFY_ID=your-id
+SPOTIFY_SECRET=your-secret
+
+# Reddit API keys
+
+REDDIT_USER_AGENT=your-user-agent
+REDDIT_CLIENT_ID=your-client
+REDDIT_CLIENT_SECRET=your-client-secret
+REDDIT_USERNAME=your-username
+REDDIT_PASSWORD=your-password
+
+# OMDB API keys
+
+OMDB_KEY=your-key```
+
+The app takes the third command line argument to determine its action. Any subsequent arguments may be action parameters.
+
+Type the follwing in your command line for a list of all available actions: 
+`node liri.js help`
--- liri.js
+++ liri.js
@@ -7,7 +7,21 @@ var keys = require('./keys.js');
 var spotify = new Spotify(keys.spotify);
 var reddit = new Reddit(keys.reddit);
 
-if (process.argv[2] === "spotify-this-song") {
+if (process.argv[2] === "help") {
+    console.log('Here are the currently available commands:')
+    console.log();
+    console.log('node liri.js spotify-this-song "Song Name Here"');
+    console.log('--> Searches Spotify for a song with that title.');
+    console.log();
+    console.log('node liri.js my-comments #');
+    console.log('--> Searches Reddit for user comments.');
+    console.log('----> An integer argument is optional. If one is supplied, it sets the maximum number of results. It is 20 by default.')
+    console.log();
+    
+    console.log('node liri.js help');
+    console.log('--> Provides a list of available options. But you already knew that ;)');
+}
+else if (process.argv[2] === "spotify-this-song") {
     spotify
         .search({ type: 'track', query: process.argv[3] })
         .then(function (response) {
@@ -22,9 +36,11 @@ if (process.argv[2] === "spotify-this-song") {
         });
 }
 
-if (process.argv[2] === "my-comments") {
+else if (process.argv[2] === "my-comments") {
     reddit.getUser(reddit.username).getComments().then(function (res) {
-        for(var i=0; i<res.length; i++){
+        var numberToDisplay = parseInt(process.argv[3]) || 20;
+        if (numberToDisplay > res.length || numberToDisplay < 0 ) { numberToDisplay = res.length; }
+        for(var i=0; i<numberToDisplay; i++){
             console.log(res[i].body);
             console.log(new Date(new Date(parseInt(res[i].created_utc)*1000) + ' UTC'));
         }
